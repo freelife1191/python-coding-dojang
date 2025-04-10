d93a76170195816a8ef88b608b36831759a80fe2
mskwon <mskwon@tidesquare.com>
2025-04-11 08:58:59 +0900

    2025.04.11 공부

---
+++ unit05/_5_1.py
@@ -0,0 +1,17 @@
+print(4//2.0)
+# 2.0
+print(2 ** 10)
+# 1024
+print(type(10))
+# <class 'int'>
+print(divmod(5, 2))
+# (2, 1) 몫과 나머지
+quotient, remainder = divmod(5, 2)
+print(quotient, remainder)
+# 2 1
+print(0b110) # 2진수 0~1
+# 6
+print(0o10) # 8진수 0~7
+# 8
+print(0xF) # 16진수 0~9 A~F
+# 15
---
+++ unit05/_5_2.py
@@ -0,0 +1,3 @@
+print(complex(1.2, 1.3)) # 복소수
+# # (1.2+1.3j)
+print(float(10//3))
